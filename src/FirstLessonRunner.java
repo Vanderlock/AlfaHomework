580dae1c0a646a885047bed79702c5122114cbe4
Vanderlock <valendor70@gmail.com>
2026-04-13 10:55:41 +0300

    add solution

--- src/FirstLessonRunner.java
+++ src/FirstLessonRunner.java
@@ -1,5 +1,21 @@
 public class FirstLessonRunner {
     static void main() {
-        System.out.println("Здарова, мир!");
+        String name = "Игорь";
+        String position = "Шаурмэн";
+        int shiftRate = 4000;
+        int shift = 22;
+        int bonus = 1000;
+        int fine = 50;
+        int price = 200;
+        int quantity = 108;
+
+        //Решение
+        System.out.println("Сотрудник: " + name);
+        System.out.println("Должность: " + position);
+        System.out.println("Оплата за смены: " + shiftRate * shift);
+        System.out.println("Премия: " + bonus);
+        System.out.println("Штраф: " + fine);
+        System.out.println("Итоговая зарплата за месяц: " + (shiftRate * shift + bonus - fine));
+        System.out.println("Выручка шаурмы за день: " + price * quantity);
     }
 }
